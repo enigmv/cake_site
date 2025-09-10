17d35a9a70e7f83cbe93c46dc1654963ad16d82c
enigmv <popo95048@gmail.com>
2025-09-10 20:17:51 +0300

    chat beta

--- cakeshop/cakeshop/settings.py
+++ cakeshop/cakeshop/settings.py
@@ -138,7 +138,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
-    BASE_DIR  / "staticfiles",
+    BASE_DIR / "main" / "static",
 ]
 STATIC_ROOT = BASE_DIR  / "staticfiles"
 
