21cf569bce1f90a44f7788d37f6ad2d35d143898
enigmv <popo95048@gmail.com>
2025-09-18 21:10:33 +0300

    chat beta1

--- cakeshop/cakeshop/asgi.py
+++ cakeshop/cakeshop/asgi.py
@@ -1,8 +1,14 @@
 import os
+import sys
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
+
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 import chat.routing
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cakeshop.settings")
 
