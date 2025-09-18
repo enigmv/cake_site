e0a79159aacba6b732fd18c1dba4c667af4dc8cd
enigmv <popo95048@gmail.com>
2025-09-18 21:16:19 +0300

    chat beta1

--- cakeshop/cakeshop/asgi.py
+++ cakeshop/cakeshop/asgi.py
@@ -4,16 +4,19 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
-
-sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-
-import chat.routing
-
-
+# Устанавливаем DJANGO_SETTINGS_MODULE до импорта chat
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cakeshop.settings")
 
+# добавляем корень проекта в PYTHONPATH
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+# вызываем get_asgi_application чтобы инициализировать Django
+django_asgi_app = get_asgi_application()
+
+import chat.routing  # теперь можно импортировать модели
+
 application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
+    "http": django_asgi_app,
     "websocket": AuthMiddlewareStack(
         URLRouter(
             chat.routing.websocket_urlpatterns
